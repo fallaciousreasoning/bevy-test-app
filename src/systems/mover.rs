3c6050474ea1255e575cbd84b64cf41622969fa7
Jay Harris <jay.harris@outlook.co.nz>
2021-04-07 17:26:32 +0200

    Use delta time to apply speed

--- src/systems/mover.rs
+++ src/systems/mover.rs
@@ -1,10 +1,10 @@
-use bevy::prelude::{Query, Transform};
+use bevy::prelude::{Query, Transform, Res, Time};
 use super::components::Velocity;
 
-pub fn mover(mut query: Query<(&Velocity, &mut Transform)>) {
+pub fn mover(time: Res<Time>, mut query: Query<(&Velocity, &mut Transform)>) {
     for (velocity, mut transform) in query.iter_mut() {
        let translation = &mut transform.translation;
-        translation.x += velocity.x;
-        translation.y += velocity.y;
+        translation.x += velocity.x * time.delta_seconds();
+        translation.y += velocity.y * time.delta_seconds();
     }
 }
