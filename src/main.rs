f924e0529fe610c802bfca05476111fc5cdc4b9d
Jay Harris <jay.harris@outlook.co.nz>
2021-04-07 16:56:45 +0200

    Add entity to world

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ pub mod systems;
 
 fn main() {
     App::build()
+        .add_startup_system(initialize_world.system())
         .add_system(systems::mover.system())
         .run();
+}
+
+fn initialize_world(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(components::Position { x: 6.0, y: 7.0 });
 }
