caf6c0b868b200c6253f7c58539d0452aadeb24b
Jay Harris <jay.harris@outlook.co.nz>
2021-04-07 17:04:46 +0200

    Attempt to load sprite

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,18 @@ fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_startup_system(initialize_world.system())
-        .add_system(systems::mover.system())
+        // .add_system(systems::mover.system())
         .run();
 }
 
-fn initialize_world(mut commands: Commands) {
+fn initialize_world(mut commands: Commands, asset_server: Res<AssetServer>, mut materials: ResMut<Assets<ColorMaterial>>) {
+    let texture_handle = asset_server.load("square.png");
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(texture_handle.into()),
+        ..Default::default()
+    });
+
     commands
         .spawn()
         .insert(components::Position { x: 6.0, y: 7.0 });
