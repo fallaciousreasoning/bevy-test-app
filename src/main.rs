e780c8411d06448038510df6c077a627cd34a2e8
Jay Harris <jay.harris@outlook.co.nz>
2021-04-08 16:30:16 +0200

    Change scale

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use bevy_rapier2d::physics::RapierPhysicsPlugin;
+use bevy_rapier2d::{physics::RapierPhysicsPlugin, rapier::{dynamics::RigidBodyBuilder, geometry::ColliderBuilder}};
 
 pub mod components;
 pub mod systems;
@@ -9,22 +9,34 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin)
         .add_startup_system(initialize_world.system())
+        .add_startup_system(make_wall.system())
         .add_system(systems::mover.system())
         .add_system(systems::player_controller.system())
         .run();
 }
 
-fn initialize_world(
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+fn initialize_world(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.transform.scale = Vec3::new(1.0/64.0, 1.0/64.0, 1.0);
+    commands.spawn_bundle(camera);
+
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
-            sprite: Sprite::new(Vec2::new(64.0, 64.0)),
+            sprite: Sprite::new(Vec2::new(1.0, 1.0)),
             ..Default::default()
         })
         .insert(components::Velocity { x: 0.0, y: 0.0 })
-        .insert(components::Character { speed: 200.0 });
+        .insert(components::Character { speed: 10.0 });
+}
+
+fn make_wall(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    let body = RigidBodyBuilder::new_dynamic().gravity_scale(0.0);
+    let collider = ColliderBuilder::cuboid(0.5, 2.0);
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(1.0,1.0,1.0).into()),
+        sprite: Sprite::new(Vec2::new(1.0,4.0)),
+        transform: Transform::from_xyz(5.0, 0.0, 0.0),
+        ..Default::default()
+    }).insert(body).insert(collider);
 }
