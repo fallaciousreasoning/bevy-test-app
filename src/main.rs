641d142f167f593b6a89ae6c7c097bce23e3e6df
Jay Harris <jay.harris@outlook.co.nz>
2021-04-08 17:27:09 +0200

    WIppy ness

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin)
         .add_startup_system(initialize_world.system())
-        .add_startup_system(make_wall.system())
+        .add_startup_system(make_walls.system())
         .add_startup_system(make_player.system())
         .add_system(systems::mover.system())
         .add_system(systems::player_controller.system())
@@ -22,16 +22,19 @@ fn main() {
 fn initialize_world(
     mut commands: Commands,
     mut rapier_config: ResMut<RapierConfiguration>,
+    materials: ResMut<Assets<ColorMaterial>>,
 ) {
     rapier_config.scale = 1.0;
     rapier_config.gravity = Vector2::zeros();
     let mut camera = OrthographicCameraBundle::new_2d();
     camera.transform.scale = Vec3::new(1.0 / 64.0, 1.0 / 64.0, 1.0);
     commands.spawn_bundle(camera);
+
+    make_box(1.0, 4.0, 4.0, 0.0, true, commands, materials)
 }
 
 fn make_player(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
-    let body = RigidBodyBuilder::new_dynamic().gravity_scale(0.0);
+    let body = RigidBodyBuilder::new_dynamic();
     let collider = ColliderBuilder::cuboid(0.5, 0.5);
     commands
         .spawn_bundle(SpriteBundle {
@@ -45,16 +48,31 @@ fn make_player(mut commands: Commands, mut materials: ResMut<Assets<ColorMateria
         .insert(collider);
 }
 
-fn make_wall(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
-    let body = RigidBodyBuilder::new_dynamic().gravity_scale(0.0);
-    let collider = ColliderBuilder::cuboid(0.5, 2.0);
+fn make_box(
+    width: f32,
+    height: f32,
+    x: f32,
+    y: f32,
+    dynamic: bool,
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let body = if dynamic {
+        RigidBodyBuilder::new_dynamic()
+    } else {
+        RigidBodyBuilder::new_static()
+    }.translation(x, y);
+    let collider = ColliderBuilder::cuboid(width / 2.0, height / 2.0);
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
-            sprite: Sprite::new(Vec2::new(1.0, 4.0)),
-            transform: Transform::from_xyz(5.0, 0.0, 0.0),
+            sprite: Sprite::new(Vec2::new(width, height)),
             ..Default::default()
         })
         .insert(body)
         .insert(collider);
+}
+
+fn make_walls(commands: Commands, materials: ResMut<Assets<ColorMaterial>>) {
+    make_box(1.0, 1000.0, -10.0, 0.0, false, commands, materials);
 }
