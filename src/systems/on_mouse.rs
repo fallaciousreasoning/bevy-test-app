faaaedf0214a005224a0fedd39fada710ba98490
Jay Harris <jay.harris@outlook.co.nz>
2021-04-14 16:46:03 +0200

    Fix cursor position

--- src/systems/on_mouse.rs
+++ src/systems/on_mouse.rs
@@ -1,13 +1,14 @@
-use bevy::{prelude::{Query, Transform, Res}, window::Windows};
+use bevy::{prelude::{Query, Transform, Res, Vec2}, window::Windows};
 
 use crate::components::OnMouse;
 
 pub fn on_mouse(windows: Res<Windows>, mut query: Query<(&OnMouse, &mut Transform)>) {
-    let mouse_position = if let Some(cursor_position) = windows.get_primary().and_then(|window| window.cursor_position()) {
+    let primary_window = windows.get_primary();
+    let mouse_position = if let Some(cursor_position) = primary_window.and_then(|window| window.cursor_position()) {
         cursor_position
     } else {
         return;
-    } / 64.;
+    } / 64. - primary_window.and_then(|w| Some(Vec2::new(w.width(), w.height()) / 2. / 64.)).unwrap();
 
     for (_mouse, mut transform) in query.iter_mut() {
         transform.translation.x = mouse_position.x;
